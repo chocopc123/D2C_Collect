176579d83ea860a22761cbcf2854854544040e5a
choco <0215ryuuku@gmail.com>
2020-03-08 17:55:57 +0900

    shops/createアクション作成

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -2,4 +2,17 @@ class ShopsController < ApplicationController
   def index
     @shops = Shop.all.order(id: :desc)
   end
+
+  def new
+  end
+
+  def create
+    @shop = Shop.new(name: params[:name], url: params[:url], icon_name: "default_icon.jpg")
+    if @shop.save
+      flash[:notice] = "登録完了しました"
+      redirect_to("/shops/index")
+    else
+      render("shops/new")
+    end
+  end
 end
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,2 +1,4 @@
 class Shop < ApplicationRecord
+    validates :name, {presence: true, uniqueness: true}
+    validates :url, {presence: true, uniqueness: true}
 end
