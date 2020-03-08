e84b180daae791f8f03ec043e8295943ea875dd9
choco <0215ryuuku@gmail.com>
2020-03-08 17:02:40 +0900

    ショップデータベース、shops/index作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get "/" => "home#top"
+  get "shops/index" => "shops#index"
   get "about" => "home#about"
+  get "/" => "home#top"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20200308073057_add_url_column_to_shop.rb
@@ -0,0 +1,5 @@
+class AddUrlColumnToShop < ActiveRecord::Migration[5.2]
+  def change
+    add_column :shops, :url, :string
+  end
+end
