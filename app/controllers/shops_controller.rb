51f2c9440fcc803a1350570d425e260021464787
choco <0215ryuuku@gmail.com>
2020-03-10 03:18:25 +0900

    ショップアイコン投稿機能作成

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -13,6 +13,12 @@ class ShopsController < ApplicationController
   def create
     @shop = Shop.new(name: params[:name], url: params[:url], icon_name: "default_icon.jpg")
     if @shop.save
+      if params[:icon]
+        @shop.icon_name = "#{@shop.id}.jpg"
+        icon = params[:icon]
+        File.binwrite("public/shop_icons/#{@shop.icon_name}", icon.read)
+        @shop.save
+      end
       flash[:notice] = "登録完了しました"
       redirect_to("/shops/index")
     else
