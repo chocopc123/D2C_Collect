d157509cf8a215b63da465e5e20242b3518010b6
choco <0215ryuuku@gmail.com>
2020-03-12 18:52:03 +0900

    home/owner作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
   post "shops/create" => "shops#create"
   get "shops/new" => "shops#new"
   get "shops/index" => "shops#index"
+  get "shops/login_form" => "shops#login_form"
   get "shops/:id" => "shops#show"
   get "shops/:id/edit" => "shops#edit"
   post "shops/:id/update" => "shops#update"
   post "shops/:id/destroy" => "shops#destroy"
+  get "owner" => "home#owner"
   get "about" => "home#about"
   get "/" => "home#top"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
