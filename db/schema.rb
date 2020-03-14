9932af29f81aee5e4e2e7e746852ee9a09180d99
choco <0215ryuuku@gmail.com>
2020-03-14 20:36:59 +0900

    Adminテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_12_074736) do
+ActiveRecord::Schema.define(version: 2020_03_14_113240) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string "admin_id"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "shops", force: :cascade do |t|
     t.string "name"
