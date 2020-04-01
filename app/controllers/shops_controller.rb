afc151152770b842e9a8adca33c167d3bc70ef94
choco <0215ryuuku@gmail.com>
2020-04-01 23:59:30 +0900

    ジャンル検索機能修正(未完成)

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -20,19 +20,22 @@ class ShopsController < ApplicationController
         @shops = Shop.where("name Like ?", "%#{params[:keyword]}%").order(id: :desc)
       else
         shops_id = ShopsGenre.where(genre_id: @search_genre.genre_id)
-        if (shops_id.to_a)[0] != nil
-          @shops = []
-        #  @shops = Shop.where(id: (shops_id.to_a)[0].shop_id )
-          @shops[0] = Shop.where(id: (shops_id.to_a)[0] )
-          @shops[1] = Shop.where(id: (shops_id.to_a)[1] )
-        end
         #if (shops_id.to_a)[0] != nil
         #  @shops = []
-        #  i = 0
-        #  (shops_id.to_a).each do |shops_id|
-        #    @shops[i] = Shop.where(id: shops_id.shop_id )
-        #  end
+        #  @shops
+
+        #  @shops = Shop.where(id: (shops_id.to_a)[0].shop_id )
+        #  @shops[0] = Shop.where(id: (shops_id.to_a)[0] )
+        #  @shops[1] = Shop.where(id: (shops_id.to_a)[1] )
         #end
+        if (shops_id.to_a)[0] != nil
+          @shops = []
+          i = 0
+          (shops_id.to_a).each do |shops_id|
+            @shopsx = Shop.where(id: shops_id.shop_id )
+            @shops + [@shopsx]
+          end
+        end
 
       end
     else
