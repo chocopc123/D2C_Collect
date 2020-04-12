1246ac951079c13d50c3f52ee795836822d7fbaf
choco <0215ryuuku@gmail.com>
2020-04-12 17:49:33 +0900

    sessions/new編集

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     get 'users/login', to: 'devise/sessions#new'
     post 'users/login', to: 'devise/sessions#create'
+    delete 'users/logout', to: 'devise/sessions#destroy'
   end
   post "shops/create" => "shops#create"
   get "shops/new" => "shops#new"
