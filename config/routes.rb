4c87ec5a576b247f52f9ad1ac9893ce0a25cbbc4
choco <0215ryuuku@gmail.com>
2020-04-13 17:41:27 +0900

    Revert "rails g devise:controllers users"
    This reverts commit 72d4334f50dc35103144a5d639dad7f1029443fa.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  devise_scope :user do
-    get 'users/login', to: 'devise/sessions#new'
-    post 'users/login', to: 'devise/sessions#create'
-  end
   post "shops/create" => "shops#create"
   get "shops/new" => "shops#new"
   get "shops/index" => "shops#index"
