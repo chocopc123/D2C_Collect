1a0156cde642f606d140a6a6ba78789dd4b54614
choco <0215ryuuku@gmail.com>
2020-04-16 22:34:44 +0900

    usersにbeforeaction設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,47 +1,56 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user, {only: [:edit, :logout]}
+  before_action :ensure_correct_user, {only: [:edit]}
 
-    def new
-        @user = User.new
-    end
-
-    def create
-        @user = User.new(username: params[:username], email: params[:email], password: params[:password], icon_name: "default_icon.jpg")
-        if @user.save
-          if params[:icon]
-            @user.icon_name = "#{@user.id}.jpg"
-            icon = params[:icon]
-            File.binwrite("public/user_icons/#{@user.icon_name}", icon.read)
-            @user.save
-          end
-          session[:user_id] = @user.id
-          flash[:notice] = "登録完了しました"
-          redirect_to("/users/index")
-        else
-          render("users/new")
-        end
-    end
-
-    def login
-      @user = User.find_by(email: params[:email])
-      if @user && @user.authenticate(params[:password])
-        session[:user_id] = @user.id
-        flash[:notice] = "ログインしました"
-        redirect_to("/users/#{@user.id}")
-      else
-        @email = params[:email]
-        @password = params[:password]
-        @error_message = "メールアドレスまたはパスワードが間違っています"
-        render("users/login_form")
-      end
-    end
-
-    def logout
-      session[:user_id] = nil
-      flash[:notice] = "ログアウトしました"
+  def ensure_correct_user
+    if @current_user && @current_user.id != params[:id].to_i
+      flash[:notice] = "権限がありません"
       redirect_to("/shops/index")
     end
+  end
 
-    def show
-      @user = User.find_by(id: params[:id])
+  def new
+      @user = User.new
+  end
+
+  def create
+      @user = User.new(username: params[:username], email: params[:email], password: params[:password], icon_name: "default_icon.jpg")
+      if @user.save
+        if params[:icon]
+          @user.icon_name = "#{@user.id}.jpg"
+          icon = params[:icon]
+          File.binwrite("public/user_icons/#{@user.icon_name}", icon.read)
+          @user.save
+        end
+        session[:user_id] = @user.id
+        flash[:notice] = "登録完了しました"
+        redirect_to("/shops/index")
+      else
+        render("users/new")
+      end
+  end
+
+  def login
+    @user = User.find_by(email: params[:email])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      flash[:notice] = "ログインしました"
+      redirect_to("/users/#{@user.id}")
+    else
+      @email = params[:email]
+      @password = params[:password]
+      @error_message = "メールアドレスまたはパスワードが間違っています"
+      render("users/login_form")
     end
+  end
+
+  def logout
+    session[:user_id] = nil
+    flash[:notice] = "ログアウトしました"
+    redirect_to("/shops/index")
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+  end
 end
